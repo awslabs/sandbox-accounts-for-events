9c3ea803d7f94372a1456f50cd7519281b8ffa8d
Rainer Moeller <moellr@amazon.de>
2022-07-29 20:39:27 +0200

    added script comments

--- dce-integration/scripts/terraform-destroy.sh
+++ dce-integration/scripts/terraform-destroy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+echo "Downloading TerraForm state file from S3 bucket."
 aws s3 cp s3://$TERRAFORM_BUCKET/terraform.state.zip .
 
 if [ "$?" -eq 0 ]
 then 
+    echo "Unzipping TerraForm state file." &&
     unzip terraform.state.zip && 
+    echo "Initializing TerraForm run." &&
     terraform init && 
+    echo "Destroying TerraForm resources." &&
     terraform destroy -auto-approve -var-file dce.tfvars &&
+    echo "Emptying TerraForm state S3 bucket." &&
     aws s3 rm s3://$TERRAFORM_BUCKET --recursive
 else
     echo "Download of TerraForm state file failed. Assuming automated rollback, so setting this execution to SUCCEEDED."
